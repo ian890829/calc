1e1ef4160d1f65a2c87ca982952a8de7861f10c2
Ian <ooo6312970@gmail.com>
2025-10-19 22:10:10 +0800

    divide test completed

--- Calc.py
+++ Calc.py
@@ -3,6 +3,7 @@ class Calculator:
         return a + b
     def sub(self, a, b):
         return a - b
-    
     def multiply(self, a, b):
         return a * b
+    def divide(self, a, b):
+        return a // b  # Integer division
