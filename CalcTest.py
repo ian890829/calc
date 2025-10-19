804e27a265dafbb88b86cdc41ac8ed929344dcdf
Ian <ooo6312970@gmail.com>
2025-10-19 21:52:24 +0800

    test sub added

--- CalcTest.py
+++ CalcTest.py
@@ -1,11 +1,16 @@
 import unittest
-from calculator import Calculator  # The class we are going to implement
+from Calc import Calculator  # The class we are going to implement
 
 class TestCalculator(unittest.TestCase):
     def test_add(self):
         calc = Calculator()
         result = calc.add(2, 3)
         self.assertEqual(result, 5)  # Expect 2 + 3 = 5
+
+    def test_subtract(self):
+        calc = Calculator()
+        result = calc.subtract(5, 3)
+        self.assertEqual(result, 2)  # Expect 5 - 3 = 2
 
 if __name__ == "__main__":
     unittest.main()
