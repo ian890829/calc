181e13af7570952fc0b56a21b3efdb40980675a4
Ian <ooo6312970@gmail.com>
2025-10-19 22:02:54 +0800

    sub function completed

--- CalcTest.py
+++ CalcTest.py
@@ -9,7 +9,7 @@ class TestCalculator(unittest.TestCase):
 
     def test_subtract(self):
         calc = Calculator()
-        result = calc.subtract(5, 3)
+        result = calc.sub(5, 3)
         self.assertEqual(result, 2)  # Expect 5 - 3 = 2
 
 if __name__ == "__main__":
